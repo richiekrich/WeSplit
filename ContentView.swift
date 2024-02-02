1e364f2b1506e724e20b129da2810a38a88ea739
richiekrich <117860505+richiekrich@users.noreply.github.com>
2024-02-02 09:34:27 -0600

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,15 @@
+//
+// WeSplitApp.swift
+// WeSplit
+//
+// Created by Richard Rich on 2/1/24.
+//
+import SwiftUI
+@main
+struct WeSplitApp: App {
+var body: some Scene {
+WindowGroup {
+ContentView()
+}
+}
+}
